f39fcf7c6b5c25746fedf0229476ef36a8b918bf
Julien Déoux <julien.deoux@telecomnancy.net>
2017-03-14 16:38:21 +0100

    More flexible querier

--- querier.py
+++ querier.py
@@ -4,30 +4,40 @@ class Querier():
     """Wrapper class for blazegraph-python"""
     def __init__(self, url):
         self.server = sparql.SPARQLServer(url)
+        self.prefixes = {}
 
     def query(self, text):
         finalQuery = ""
-        prefixes = {}
-        csv = "predicate,object,subject\n"
+        args = []
+        csv = ""
         lines = text.split("\n")
         for line in lines:
             if "PREFIX" in line:
                 blocks = line.split(" ")
                 prefix = blocks[1]
                 uri = blocks[2].replace('<', '').replace('>', '')
-                prefixes[uri] = prefix
+                self.prefixes[uri] = prefix
             else:
                 finalQuery = finalQuery + "\n" + line
+                if "SELECT" in line:
+                    for word in line.split(' '):
+                        if word[0] == '?':
+                            args.append(word[1:])
+        for arg in args:
+            csv = csv + arg + ','
+        csv = csv[:len(csv)-1] + '\n'
         print "Querying server"
         result = self.server.query(text)
+        print "Done"
         for b in result['results']['bindings']:
-            predicate = self.replacePrefix(b['predicate']['value'], prefixes)
-            obj = self.replacePrefix(b['object']['value'], prefixes)
-            subject = self.replacePrefix(b['subject']['value'], prefixes)
-            csv = csv + predicate + ',' + obj + ',' + subject + '\n'
+            line = ""
+            for arg in args:
+                line = line + self.replacePrefix(b[arg]['value']) + ','
+            line = line[:len(line)-1] + '\n'
+            csv = csv + line
         return csv
 
-    def replacePrefix(self, text, prefixes):
-        for key in prefixes:
-            text = text.replace(key, prefixes[key])
+    def replacePrefix(self, text):
+        for key in self.prefixes:
+            text = text.replace(key, self.prefixes[key])
         return text
