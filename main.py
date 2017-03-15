3f8bc2c8137f37be66466486d7e741c5b275b278
isaaz <eldendron25@hotmail.fr>
2017-03-15 09:52:44 +0100

    modif pour que le main fonctionne

--- main.py
+++ main.py
@@ -10,23 +10,27 @@ localPropertyFinder = PropertyFinder(localQuerier)
 
 effectsList = [{'effect': "", 'disease': ""}]
 
-csvFile = open("result.tsv")
-trainingSet = DictReader(csvFile)[:TRAINING_SAMPLE_SIZE]
+csvFile = open("result.csv")
+trainingSet = DictReader(csvFile)
+
 
 trainingRows = []
 trainingData = [[0 for i in range(len(effectsList))] for j in range(TRAINING_SAMPLE_SIZE)]
 
-for index1, row in trainingSet:
-    geneProperties = DictReader(localPropertyFinder.findGeneProperties(row['gene']))
-    for prop in geneProperties:
-        for index2, item in effectsList:
-            if prop == item:
-                trainingData[index1][index2] = 1
-    drugProperties = DictReader(localPropertyFinder.findDrugProperties(row['drug']))
-    for prop in drugProperties:
-        for index2, item in effectsList:
-            if prop == item:
-                trainingData[index1][index2] = 1
+index1=0
+for row in  trainingSet:
+    if index1 < TRAINING_SAMPLE_SIZE :
+        geneProperties = DictReader(localPropertyFinder.findGeneProperties(row['gene']))
+        for prop in geneProperties:
+            for index2, item in effectsList:
+                if prop == item:
+                    trainingData[index1][index2] = 1
+        drugProperties = DictReader(localPropertyFinder.findDrugProperties(row['drug']))
+        for prop in drugProperties:
+            for index2, item in effectsList:
+                if prop == item:
+                    trainingData[index1][index2] = 1
+    index1 = index1 + 1
 
 rbm = RBM(num_visible=len(effectsList), num_hidden=30)
 rbm.train(np.array(trainingData), max_epochs = 300)
